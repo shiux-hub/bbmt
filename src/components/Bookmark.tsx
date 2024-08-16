2a06e1d1150f4e963f5a5ed8d037fa6e8297c87f
Shiux <48909759+shiux-lab@users.noreply.github.com>
2024-08-16 23:36:36 +0800

    fix(controller): request cancel

--- src/components/Bookmark.tsx
+++ src/components/Bookmark.tsx
@@ -1,59 +1,83 @@
-import { IconClock, IconClose, IconFolder, IconLink, IconLoading } from '@douyinfe/semi-icons'
+import { IconClock, IconClose, IconLink, IconLoading } from '@douyinfe/semi-icons'
 import { IconCheckbox } from '@douyinfe/semi-icons-lab'
-import { Badge, Breadcrumb, Button, Tree, Typography } from '@douyinfe/semi-ui'
+import { Badge, Button, Tree, Typography } from '@douyinfe/semi-ui'
 import type { TreeNodeData } from '@douyinfe/semi-ui/lib/es/tree'
 import type { AxiosError, AxiosRequestConfig, CancelTokenSource } from 'axios'
 import axios from 'axios'
-import { useState } from 'react'
+import { useRef, useState } from 'react'
+import { isArray } from 'radash'
 import { RequestStatus } from '~utils/enums'
 import instance from '~utils/request'
 
 function Bookmark({ tree }: {
   tree?: chrome.bookmarks.BookmarkTreeNode[]
 }) {
-  function getTreeData(children: chrome.bookmarks.BookmarkTreeNode[], breadcrumbs?: string[]): TreeNodeData[] {
-    const title = breadcrumbs || []
+  // const [checkedValues, setCheckedValues] = useState<string[]>()
+  // const [startCheck, setStartCheck] = useState(false)
+
+  function getTreeData(children: chrome.bookmarks.BookmarkTreeNode[]) {
     return children.map((node) => {
-      if (node.children) {
-        if (node.children.length <= 1) {
-          return {
-            label: <Item node={node} />,
-            value: node.id,
-            key: node.id
-          }
-        }
+      if (Array.isArray(node.children)) {
         return {
-          label: (
-            <div className="flex items-center gap-2">
-              <IconFolder className="text-xl" />
-              {title.length > 0
-                ? (
-                    <Breadcrumb>
-                      {title.map(breadcrumb => (
-                        <Breadcrumb.Item key={breadcrumb}>{breadcrumb}</Breadcrumb.Item>
-                      ))}
-                    </Breadcrumb>
-                  )
-                : node.title}
-            </div>
-          ),
-          value: node.id,
+          label: node.title,
           key: node.id,
-          children: node.children.length <= 1 ? getTreeData(node.children, [...breadcrumbs, node.title]) : getTreeData(node.children)
+          children: getTreeData(node.children)
         }
       }
       return {
-        label: <Item node={node} />,
-        value: node.id,
+        label: node.title,
+        value: node.url,
         key: node.id
       }
     })
   }
 
+  // function getChildren(children: TreeNodeData[]) {
+  //   let result: string[] = []
+
+  //   children.forEach((node) => {
+  //     if (isArray(node.children)) {
+  //       result = result.concat(getChildren(node.children))
+  //     }
+  //     else {
+  //       result.push(node.value as string)
+  //     }
+  //   })
+  //   return result
+  // }
+
   return (
     <Tree
       treeData={getTreeData(tree)}
       multiple
+      directory
+      filterTreeNode
+      // onChangeWithObject
+      // onChange={(value: TreeNodeData[]) => {
+      // console.log(value)
+
+      // setCheckedValues(getChildren(value))
+      // }}
+      renderLabel={(_, node) => {
+        if (isArray(node.children)) {
+          return (
+            <div className="flex items-center gap-2">
+              {node.label}
+              <Button
+                size="small"
+                onClick={(e) => {
+                  e.stopPropagation()
+                  // setStartCheck(true)
+                }}
+              >
+                开始测试
+              </Button>
+              <Badge count={node.children.length} />
+            </div>
+          )
+        }
+        return <Item node={node} />
+      }}
     />
   )
 }
@@ -62,6 +86,8 @@ function States({ type }: {
   type: RequestStatus
 }) {
   switch (type) {
+    case RequestStatus.Loading:
+      return <IconLoading className="animate-spin" />
     case RequestStatus.Succuss:
       return <IconCheckbox className="text-semi-color-success" />
     case RequestStatus.Error:
@@ -74,49 +100,58 @@ function States({ type }: {
 }
 
 function Item({ node }: {
-  node: chrome.bookmarks.BookmarkTreeNode
+  node: TreeNodeData
+  // requestStart: boolean
+  // checked: boolean
 }) {
   const [states, setStates] = useState(RequestStatus.Pending)
-  let source: CancelTokenSource
+  const source = useRef<CancelTokenSource>()
   const { Text } = Typography
+
+  function testURL() {
+    if (states === RequestStatus.Loading)
+      return source.current.cancel()
+
+    setStates(RequestStatus.Loading)
+    const closeToken = axios.CancelToken
+    source.current = closeToken.source()
+
+    const config: AxiosRequestConfig = {
+      timeout: 5000,
+      cancelToken: source.current.token
+    }
+
+    instance.get(node.value as string, config)
+      .then(() => {
+        setStates(RequestStatus.Succuss)
+      })
+      .catch((error: AxiosError) => {
+        if (error.code === 'ECONNABORTED')
+          setStates(RequestStatus.Timeout)
+        else if (axios.isCancel(error))
+          setStates(RequestStatus.Pending)
+        else
+          setStates(RequestStatus.Error)
+        console.error(error)
+      })
+  }
+
+  // if (requestStart && checked) {
+  //   testURL()
+  // }
 
   return (
     <div className="flex items-center gap-4">
       <Button
-        onClick={() => {
-          if (states === RequestStatus.Loading)
-            return source.cancel()
-
-          setStates(RequestStatus.Loading)
-          const closeToken = axios.CancelToken
-          source = closeToken.source()
-
-          const config: AxiosRequestConfig = {
-            timeout: 5000,
-            cancelToken: source.token
-          }
-
-          instance.get(node.url, config)
-            .then(() => {
-              setStates(RequestStatus.Succuss)
-            })
-            .catch((error: AxiosError) => {
-              if (error.code === 'ECONNABORTED')
-                setStates(RequestStatus.Timeout)
-              else if (axios.isCancel(error))
-                setStates(RequestStatus.Pending)
-              else
-                setStates(RequestStatus.Error)
-              console.error(error)
-            })
+        size="small"
+        onClick={(e) => {
+          e.stopPropagation()
+          testURL()
         }}
         icon={<States type={states} />}
-        loading={states === RequestStatus.Loading}
       />
-      <div>{node.title}</div>
-      <Text link={{ href: node.url, target: '_blank' }} className="font-mono" icon={<IconLink />} underline>{node.url}</Text>
-      {node.dateAdded
-      && <Badge count={node.dateAdded} />}
+      <div>{node.label}</div>
+      <Text link={{ href: node.value as string, target: '_blank' }} className="font-mono" icon={<IconLink />} underline>{node.value}</Text>
     </div>
   )
 }
--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,10 +1,34 @@
 import axios from 'axios'
 
 const instance = axios.create()
+// 请求队列
+// const queue: { url: string, cancel: CancelTokenSource['cancel'] }[] = []
+
+// 中断重复的请求，并从队列中移除
+// function removeQueue(config: AxiosRequestConfig) {
+//   for (let i = 0, size = queue.length; i < size; i++) {
+//     const task = queue[i]
+//     if (task.url === config.url) {
+//       task.cancel()
+//       queue.splice(i, 1)
+//     }
+//   }
+// }
 
 instance.interceptors.request.use((config) => {
+  // removeQueue(config)
   config.headers.set('Access-Control-Allow-Origin', '*')
+  // config.cancelToken = new axios.CancelToken((c) => {
+  //   queue.push({ url: config.url, cancel: c })
+  // })
   return config
+}, (error) => {
+  return Promise.reject(error)
 })
+
+// instance.interceptors.response.use((response) => {
+//   removeQueue(response.config)
+//   return response
+// })
 
 export default instance
